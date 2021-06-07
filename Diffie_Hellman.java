85729785b2a0b1ae02ebf6380726c896d6269d75
Enkhjin Enkhbold <enkhboen@gmail.com>
2021-06-07 17:16:36 +0200

    modified for N participants

--- Diffie_Hellman.java
+++ Diffie_Hellman.java
@@ -5,40 +5,75 @@ import java.util.*;
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
+
+		// get prime number and primitive root
 		System.out.println("Enter modulo(p)");
 		int p=sc.nextInt();
 		System.out.println("Enter primitive root of "+p);
 		int g=sc.nextInt();
-		System.out.println("Choose 1st secret no(Alice)");
-		int a=sc.nextInt();
-		System.out.println("Choose 2nd secret no(BOB)");
-		int b=sc.nextInt();
-		System.out.println("Choose 3rd secret no(Charlie)");
-		int c=sc.nextInt();
-		
-		int A = (int)Math.pow(g,a)%p;
-		int B = (int)Math.pow(g,b)%p;
-		int C = (int)Math.pow(g,c)%p;
-		
-		int AB = (int)Math.pow(B,a)%p;
-		int BC = (int)Math.pow(C,b)%p;
-		int CA = (int)Math.pow(A,c)%p;
 
-		int ABC = (int)Math.pow(BC,a)%p;
-		int BAC = (int)Math.pow(CA,b)%p;
-		int CAB = (int)Math.pow(AB,c)%p;
+		// get a number of participants
+		System.out.println("Enter number of participants: ");
+		int n=sc.nextInt();
+
+		// get secret keys
+		int[] secrets = new int[n];
+
+		for (int i = 0; i < n; ++i) {
+			System.out.println("Enter secret for participant " + i + ":");
+			// get and store secret to array
+			secrets[i] = sc.nextInt();
+		}
 
 		sc.close();
 		
-		if(ABC==BAC && ABC==CAB)
-		{
-			System.out.println("ALice, Bob and Charlie can communicate with each other!!!");
-			System.out.println("They share a secret no = "+ABC);
+		// print all secret keys
+		for (int i = 0; i < n; ++i) {
+			System.out.println("Secret key for participant "+ i + ": " + secrets[i]);
+		}
+
+		// calculate public keys
+		int[][] public_keys = new int[n][n-1];
+		int base = g;
+		int j;
+
+		for (int k = 0; k < n-1; ++k) {      // public keys
+			for (int i = 0; i < n; ++i) {    // participants
+
+				if (k != 0) {
+					j = i + 1;
+					if ( j >= n)
+						j = 0;
+					base = public_keys[j][k-1];
+				}
+
+				public_keys[i][k] = (int)Math.pow(base, secrets[i])%p;
+				//System.out.println(public_keys[i][k]);
+			}
+		}
+
+		// calculate shared keys
+		int[] shared_keys = new int[n];
+
+		for (int i = 0; i < n; ++i) {
+			j = i + 1;
+			if (j >= n)
+				j = 0;
+			shared_keys[i] = (int)Math.pow(public_keys[j][n-2],secrets[i])%p;
+
+			System.out.println("Shared secret key for participant "+ i + ": " + shared_keys[i]);
 		}
 		
-		else
-		{
-			System.out.println("ALice, Bob and Charlie cannot communicate with each other!!!");
+		// compare shared keys for compatibility
+		for (int i = 0; i < n-1; ++i) {
+			for (int k = i+1; k < n; ++k) {
+				if (shared_keys[i] == shared_keys[k]) {
+					System.out.println("Participant " + i + " and " + k + " can communicate.");
+				}
+				else {
+					System.out.println("Participant " + i + " and " +  k + " cannot communicate.");
+				}
+			}
 		}
 	}	
 }
